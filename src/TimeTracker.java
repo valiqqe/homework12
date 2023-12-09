29b1aad59e6a9379d9d584ce3bc770ede149c12e
valiqqe <113710536+valiqqe@users.noreply.github.com>
2023-12-09 16:03:41 +0200

    TimeTracker.java

--- src/TimeTracker.java
+++ src/TimeTracker.java
@@ -5,19 +5,17 @@ public class TimeTracker {
     private static int elapsedSeconds = 0;
 
     public static void main(String[] args) {
-        // Створення об'єкта Timer
+       
         Timer timer = new Timer();
 
-        // Розпочинання відліку часу кожну секунду
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 elapsedSeconds++;
                 System.out.println("Пройшло " + elapsedSeconds + " секунд");
             }
-        }, 0, 1000); // Починаємо відлік одразу, потім кожну секунду (1000 мілісекунд)
+        }, 0, 1000); 
 
-        // Створення і запуск другого потоку
         new Thread(() -> {
             while (true) {
                 try {
